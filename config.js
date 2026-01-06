ede1901014e5c710a20cd47a5e71ec1fe1129487
abasamzon <abasamzon@gmail.com>
2026-01-06 23:02:36 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+// config.js
+// === إعدادات المتجر (قم بتعديل البيانات هنا لكل زبون) ===
+
+window.MY_STORE_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBwzzJqPNa08mPBZNMuvExxCJzkToV65rE",
+    authDomain: "project-4623091190980149681.firebaseapp.com",
+    databaseURL: "https://project-4623091190980149681-default-rtdb.firebaseio.com",
+    projectId: "project-4623091190980149681",
+    storageBucket: "project-4623091190980149681.firebasestorage.app",
+    messagingSenderId: "252053182084",
+    appId: "1:252053182084:web:b35a5e3c7d6777e79a94d1",
+    measurementId: "G-H7JJBWJGQR"
+  },
+
+  cloudinary: {
+    cloudName: "dongmlmoj",
+    uploadPreset: "app_upload"
+  },
+
+  security: {
+    adminCode: "1001"
+  }
+};
